af7cd2bc7d11b149942ce7c6a9725c049612b02a
marcel <demdike12@gmail.com>
2026-03-30 17:35:02 -0500

    TDD: Test validar vacantes disponibles (falla el test)

--- src/test/java/T1/RegistroMatriculaTest.java
+++ src/test/java/T1/RegistroMatriculaTest.java
@@ -15,9 +15,19 @@ class RegistroMatriculaTest {
     void deberiaValidarLimiteCreditosAlumnoNuevo() {
         ServicioAcademico servicioMock = Mockito.mock(ServicioAcademico.class);
         RegistroMatricula registro = new RegistroMatricula(servicioMock);
-        List<Curso> cursos = List.of(new Curso("C1", 10), new Curso("C2", 5)); // 15 créditos
-
+        List<Curso> cursos = List.of(new Curso("C1", 10), new Curso("C2", 5));
         String resultado = registro.matricular("A01", cursos, true);
         assertEquals("Límite de créditos excedido para alumno nuevo", resultado);
+    }
+
+    @Test
+    @DisplayName("Debería validar vacantes disponibles")
+    void deberiaValidarVacantesDisponibles() {
+        ServicioAcademico servicioMock = Mockito.mock(ServicioAcademico.class);
+        Mockito.when(servicioMock.tieneVacantes("C1")).thenReturn(false);
+        RegistroMatricula registro = new RegistroMatricula(servicioMock);
+        List<Curso> cursos = List.of(new Curso("C1", 5));
+        String resultado = registro.matricular("A01", cursos, false);
+        assertEquals("No hay vacantes disponibles", resultado);
     }
 }
